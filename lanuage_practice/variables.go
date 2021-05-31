bcd8657164c130f6d149a9ab188a8073ec57d8b1
Ziaur Rahman <zrshishir@gmail.com>
2021-06-01 01:12:43 +0600

    variables

---
+++ lanuage_practice/variables.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a = "string"
+	fmt.Println(a)
+
+	var b, c int = 2, 3
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	f := "zrshishir"
+	fmt.Println(f)
+}
