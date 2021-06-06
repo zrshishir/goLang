30640b2a03662bbdb3539d714f7003422cf8afab
Ziaur Rahman <zrshishir@gmail.com>
2021-06-06 19:52:46 +0600

    struct

---
+++ lanuage_practice/struct.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type person struct {
+	name string
+	age  int
+}
+
+func newPerson(name string) *person {
+	p := person{name: name}
+	p.age = 42
+	return &p
+}
+
+func main() {
+	fmt.Println(person{"Bob", 20})
+	fmt.Println(person{name: "Johm", age: 29})
+	fmt.Println(person{name: "Fred"})
+}
