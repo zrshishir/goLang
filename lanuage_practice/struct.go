523f208e7539e231080784bf5af9896eb4b20474
Ziaur Rahman <zrshishir@gmail.com>
2021-06-07 07:39:42 +0600

    struct ext

--- lanuage_practice/struct.go
+++ lanuage_practice/struct.go
@@ -17,4 +17,15 @@ func main() {
 	fmt.Println(person{"Bob", 20})
 	fmt.Println(person{name: "Johm", age: 29})
 	fmt.Println(person{name: "Fred"})
+
+	fmt.Println(&person{name: "Ann", age: 40})
+	fmt.Println(newPerson("JohnD"))
+
+	s := person{name: "Sean", age: 30}
+	fmt.Println(s.name, s.age)
+
+	sp := &s
+	fmt.Println(sp.age)
+	sp.age = 35
+	fmt.Println(sp.age)
 }
