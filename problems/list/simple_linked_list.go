5f2460e340d7c07844cce98f3492bc21856e97b0
zrshishir <zrshishir@gmail.com>
2021-11-17 08:39:36 +0600

    getting text input

---
+++ problems/list/simple_linked_list.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+// @ Todo: https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem?isFullScreen=true
+
+func main()  {
+	fmt.Println("Enter text")
+	var input string
+	fmt.Scanln(&input)
+	fmt.Print(input)
+}
