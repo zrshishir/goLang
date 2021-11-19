e5f782b40adbf34971d1979bebb3885638e0a84f
zrshishir <zrshishir@gmail.com>
2021-11-19 19:18:00 +0600

    traversing simple linked list

--- problems/list/simple_linked_list.go
+++ problems/list/simple_linked_list.go
@@ -1,12 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"container/list"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 // @ Todo: https://www.hackerrank.com/challenges/print-the-elements-of-a-linked-list/problem?isFullScreen=true
 
 func main()  {
-	fmt.Println("Enter text")
-	var input string
-	fmt.Scanln(&input)
-	fmt.Print(input)
+	var iterator, i int64
+	var listVal list.List
+
+	fmt.Println("Input iterator")
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	iterator, _ = strconv.ParseInt(scanner.Text(), 10, 64)
+
+	for i = 1; i <= iterator; i++ {
+		scanner.Scan()
+		input, _ := strconv.ParseInt(scanner.Text(), 10, 64)
+		listVal.PushBack(input)
+	}
+
+	fmt.Println("Output")
+	for element := listVal.Front(); element != nil; element = element.Next() {
+		fmt.Println(element.Value.(int64))
+	}
 }
