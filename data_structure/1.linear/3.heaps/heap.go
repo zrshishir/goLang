7cfc5cd3b7041e87f7e6a421a37658bd71880fc1
zrshishir <zrshishir@gmail.com>
2022-01-02 03:23:08 +0600

    heap algorithm implementation

---
+++ data_structure/1.linear/3.heaps/heap.go
@@ -0,0 +1,23 @@
+package main
+
+//IntegerHeap a type
+type IntegerHeap []int
+
+//IntegerHeap a method - get the length of IntegerHeap
+func (iHeap IntegerHeap) Len() int {
+	return len(iHeap)
+}
+
+//IntegerHeap a method - checks if the element of i index is less than of j index
+func (iHeap IntegerHeap) Less(i, j int) bool {
+	return iHeap[i] < iHeap[j]
+}
+
+//IntegerHeap a method - swaps the element of i with j
+func (iHeap IntegerHeap) Swap(i, j int) {
+	iHeap[i], iHeap[j] = iHeap[j], iHeap[i]
+}
+
+func main()  {
+
+}
