8b21df3ba547a4cba97c0b24be9b1f66c9e8311a
zrshishir <zrshishir@gmail.com>
2022-11-07 11:45:00 +0600

    defining sentences without any packages with golang

---
+++ problems/spliting_text_to_sentence/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"regexp"
+	
+)
+
+func main(){
+	txt := "This is how I tried to split a paragraph into a sentence. But, there is a problem. My paragraph includes dates like Jan.13, 2014 , words like U.S and numbers like 2.2. They all got split by the above code."
+
+	re := regexp.MustCompile("[\\!\\?\\.] +")
+
+    sentences := re.Split(txt, -1)
+    all_together := []string{}
+
+    for i := range sentences {
+        all_together = append(all_together, sentences[i])
+		fmt.Println(sentences[i])
+    }
+
+    // fmt.Println(all_together)
+}
